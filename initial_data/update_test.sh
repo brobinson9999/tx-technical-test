ade913a22f70602801118bcc6b5f6e48638f79ec
Brendon Robinson <brobinson9999@gmail.com>
2021-06-07 08:16:18 -0400

    Test update and delete actions.

--- initial_data/update_test.sh
+++ initial_data/update_test.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 
-curl -X POST -H "Content-Type: application/json" -d @update_test.json http://localhost:8000/api/product/1
+#curl -X DELETE http://localhost:8000/api/product/19
+
+curl -X PATCH -H "Content-Type: application/json" -d @update_test.json http://localhost:8000/api/product/2
